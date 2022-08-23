fc0621baaaad9422bd18f596f365dfcac121058a
Fabio Torres <bfabiobenfica@gmail.com>
2022-08-22 23:42:54 -0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to password-manager
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/password_manager/core_test.clj
@@ -0,0 +1,7 @@
+(ns password-manager.core-test
+  (:require [clojure.test :refer :all]
+            [password-manager.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
